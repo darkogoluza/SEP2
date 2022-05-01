12b5803e1ad53f084035c45e26edc4bcc34ff833
koziara <fhdorpol.pl@gmail.com>
2022-05-02 00:55:19 +0200

    Added variables and empty methods AdministratorViewController

--- RentalSystem/src/administrator/view/administratorView/AdministratorViewController.java
+++ RentalSystem/src/administrator/view/administratorView/AdministratorViewController.java
@@ -1,4 +1,60 @@
 package administrator.view.administratorView;
 
+import administrator.core.ViewHandler;
+import administrator.core.ViewModelFactory;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+
+import java.util.List;
+
+
 public class AdministratorViewController {
+
+    @FXML
+    private TextField idTextFieldAdministrator;
+    @FXML
+    private ChoiceBox typeChoiceBoxAdministrator;
+    @FXML
+    private ChoiceBox colorChoiceBoxAdministrator;
+    @FXML
+    private TextField sizeTextFieldAdministrator;
+    @FXML
+    private Button addButtonAdministrator;
+    @FXML
+    private Button removeButtonAdministrator;
+    @FXML
+    private Button editButtonAdministrator;
+    @FXML
+    private ListView listViewAdministrator;
+
+    private ViewHandler viewHandler;
+    private AdministratorViewModel administratorViewModel;
+
+    public void init(ViewHandler viewHandler, ViewModelFactory vmf)
+    {
+        this.viewHandler = viewHandler;
+
+    }
+
+    public void addButtonAdministrator(ActionEvent event)
+    {
+
+    }
+
+    public void removeButtonAdministrator(ActionEvent event)
+    {
+
+    }
+
+    public void editButtonAdministrator(ActionEvent event)
+    {
+
+    }
+
+
+
 }
--- RentalSystem/src/administrator/view/administratorView/AdministratorViewModel.java
+++ RentalSystem/src/administrator/view/administratorView/AdministratorViewModel.java
@@ -1,4 +1,11 @@
 package administrator.view.administratorView;
 
+import javafx.beans.property.StringProperty;
+import javafx.collections.ObservableList;
+
 public class AdministratorViewModel {
+
+    private ObservableList<String> listViewAdministrator;
+    private StringProperty idTextFieldAdministrator;
+
 }
