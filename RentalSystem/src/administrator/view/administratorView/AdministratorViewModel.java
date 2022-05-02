0536a5554cee0a25b4a3c5ac42c99e4b39b1ee47
xoxkoo <anton.durcak22@gmail.com>
2022-05-02 09:50:52 +0200

    view model created;

--- RentalSystem/src/administrator/view/administratorView/AdministratorViewModel.java
+++ RentalSystem/src/administrator/view/administratorView/AdministratorViewModel.java
@@ -1,11 +1,29 @@
 package administrator.view.administratorView;
 
+import administrator.model.ManageProducts;
+import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 
 public class AdministratorViewModel {
 
     private ObservableList<String> listViewAdministrator;
     private StringProperty idTextFieldAdministrator;
+	private ManageProducts model;
+
+	public AdministratorViewModel(ManageProducts model) {
+		idTextFieldAdministrator = new SimpleStringProperty();
+		listViewAdministrator = FXCollections.observableArrayList();
+		this.model = model;
+	}
+
+	public StringProperty getIdTextFieldAdministrator() {
+		return idTextFieldAdministrator;
+	}
+
+	public ObservableList getListViewAdministrator () {
+		return listViewAdministrator;
+	}
 
 }
