7126c66ed964972b20f4ae28af511c81abde0aac
xoxkoo <anton.durcak22@gmail.com>
2022-05-10 07:30:55 +0200

    java docs for core and model done

--- RentalSystem/src/client/model/ManageProductsManager.java
+++ RentalSystem/src/client/model/ManageProductsManager.java
@@ -13,23 +13,46 @@ public class ManageProductsManager implements ManageProducts {
 		changeSupport = new PropertyChangeSupport(this);
 	}
 
+	/**
+	 * Add product to list @todo change this for database when implemented
+	 * @param price
+	 * @param color
+	 * @param equipmentType
+	 * @param size
+	 */
 	@Override
 	public void add(double price, Color color, EquipmentType equipmentType, Size size) {
 		list.add(price, color, equipmentType, size);
 		changeSupport.firePropertyChange("productModified", null, list.convertToStringArrayList());
 	}
 
+	/**
+	 * Remove selected item with index from list @todo change this for database when implemented
+	 * @param index of product
+	 */
 	@Override
 	public void remove(int index) {
 		list.removeByIndex(index);
 		changeSupport.firePropertyChange("productModified", null, list.convertToStringArrayList());
 	}
 
+	/**
+	 * Get product from list @todo change this for database when implemented
+	 * @param index of product
+	 * @return Product
+	 */
 	@Override
 	public Product getProduct(int index) {
 		return list.getByIndex(index);
 	}
 
+	/**
+	 * Update price, color and size of selected product using index of product
+	 * @param index
+	 * @param newPrice
+	 * @param newColor
+	 * @param newSize
+	 */
 	@Override
 	public void changeProduct(int index, double newPrice, Color newColor, Size newSize) {
 		list.change(index, newPrice, newColor, newSize);
