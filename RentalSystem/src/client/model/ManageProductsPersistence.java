72e36d603322923fd805f807b359b0a01987baf5
koziara <fhdorpol.pl@gmail.com>
2022-05-11 09:11:13 +0200

    Opening the view for customer all eq

--- RentalSystem/src/client/model/ManageProductsPersistence.java
+++ RentalSystem/src/client/model/ManageProductsPersistence.java
@@ -8,13 +8,11 @@ import java.util.ArrayList;
 
 public interface ManageProductsPersistence
 {
-<<<<<<< HEAD
-    ProductArrayList load();
+
+    //ProductArrayList load();
     void save(ProductArrayList productArrayList) throws SQLException;
-=======
     ProductArrayList load() throws SQLException;
-    void save(ProductArrayList productArrayList);
->>>>>>> 541254b1047ace9e9ced81710ef913140e765c92
+    //void save(ProductArrayList productArrayList);
     void save(Product product) throws SQLException;
     void remove(Product product) throws SQLException;
     void clear();
