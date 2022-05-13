a9af3c06f04da6d374403da1acddf77d9510a18e
Davidek776 <kocurdavid776@gmail.com>
2022-05-13 12:06:31 +0200

    Added EmployeeOrderRDetailsViewModel

--- RentalSystem/src/client/view/EmployeeOrderDetails/EmployeeOrderDetailsViewModel.java
+++ RentalSystem/src/client/view/EmployeeOrderDetails/EmployeeOrderDetailsViewModel.java
@@ -6,13 +6,15 @@ import javafx.beans.property.SimpleListProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 import javafx.collections.FXCollections;
+import shared.objects.product.Color;
+import shared.objects.product.Size;
 
 import java.beans.PropertyChangeEvent;
 import java.util.ArrayList;
 
 public class EmployeeOrderDetailsViewModel
 {
-  private StringProperty nameOfOrderProperty;
+  private StringProperty idProperty;
   private StringProperty priceProperty;
   private StringProperty userNameProperty;
   private StringProperty uniqueCodeProperty;
@@ -23,15 +25,35 @@ public class EmployeeOrderDetailsViewModel
   public EmployeeOrderDetailsViewModel(ModelProxy modelProxy)
   {
 
-    nameOfOrderProperty = new SimpleStringProperty();
+    idProperty = new SimpleStringProperty();
     priceProperty = new SimpleStringProperty();
     userNameProperty = new SimpleStringProperty();
     uniqueCodeProperty = new SimpleStringProperty();
     phoneNumberProperty = new SimpleStringProperty();
     statusProperty = new SimpleStringProperty();
     this.modelReservations = modelProxy.getManageReservations();
-    nameOfOrderProperty.set(modelReservations.getReservation(0).getUserName());
   }
+
+  public void showId(){
+    idProperty.set(""+modelReservations.getReservation(0).getId());
+  }
+
+  public void showUserName(){
+    userNameProperty.set(modelReservations.getReservation(0).getUserName());
+  }
+
+  public void showCreatedAt(){
+    userNameProperty.set(""+modelReservations.getReservation(0).getCreatedAt());
+  }
+
+  public void showStatus(){
+    userNameProperty.set(""+modelReservations.getReservation(0).getStatus());
+  }
+
+  public void editId(int index, double price, Color color, Size size) {
+    model.changeProduct(index, price, color, size);
+  }
+
 
 
 }
--- RentalSystem/src/shared/objects/reservation/Reservation.java
+++ RentalSystem/src/shared/objects/reservation/Reservation.java
@@ -4,22 +4,23 @@ import shared.objects.product.ProductList;
 
 import java.util.Date;
 
-public class Reservation {
+public class Reservation
+{
     private int id;
     private String userName;
     private Date createdAt;
     private ProductList productList;
     private ReservationStatus status;
 
-    public Reservation(int id, String userName, ProductList products) {
+    public Reservation(int id, String userName, ProductList products)
+    {
         this.id = id;
         this.userName = userName;
         productList = products;
         status = ReservationStatus.rented;
         createdAt = new Date(System.currentTimeMillis());
-    }
 
-    private Reservation(int id, String userName, ProductList products, ReservationStatus status, Date createdAt) {
+    }private Reservation( int id, String userName, ProductList products, ReservationStatus status, Date createdAt){
         this.id = id;
         this.userName = new String(userName);
         this.productList = products.copy();
@@ -27,53 +28,55 @@ public class Reservation {
         this.createdAt = new Date(createdAt.getTime());
     }
 
-    public int getId() {
+        public int getId () {
         return id;
     }
 
-    public String getUserName() {
+        public String getUserName () {
         return userName;
     }
 
-    public Date getCreatedAt() {
+        public Date getCreatedAt () {
         return createdAt;
     }
 
-    public ReservationStatus getStatus() {
+        public ReservationStatus getStatus () {
         return status;
     }
 
-    public ProductList getProducts() {
+        public ProductList getProducts () {
         return productList;
     }
 
-    public void setStatus(ReservationStatus status) {
+        public void setStatus (ReservationStatus status){
         this.status = status;
     }
 
-    public boolean equals(Object obj) {
-        if(!(obj instanceof Reservation reservation))
+        public boolean equals (Object obj){
+        if (!(obj instanceof Reservation reservation))
             return false;
 
-        return reservation.id == id &&
-                reservation.userName.equals(userName) &&
-                reservation.createdAt.equals(createdAt) &&
-                reservation.productList.equals(productList) &&
-                reservation.status.equals(status);
+        return reservation.id == id && reservation.userName.equals(userName)
+            && reservation.createdAt.equals(createdAt) && reservation.productList.equals(productList)
+            && reservation.status.equals(status);
     }
 
-    public Reservation copy() {
+        public Reservation copy () {
         return new Reservation(id, userName, productList, status, createdAt);
     }
 
-    public String toString() {
+        public String toString () {
         StringBuilder value = new StringBuilder();
 
-        for (int i = 0; i < productList.size(); i++) {
-            value.append(String.format("id: %d\nusername: %s\ncreated at: %s\nstatus: %s\n%s", id, userName, createdAt, status, productList.toString()));
+        for (int i = 0; i < productList.size(); i++)
+        {
+            value.append(String.format(
+                "id: %d\nusername: %s\ncreated at: %s\nstatus: %s\n%s", id,
+                userName, createdAt, status, productList.toString()));
         }
 
         return value.toString();
 
     }
-}
+    }
+
