59e77841c14b75592c3ea9ee9fca85feaedb9827
xoxkoo <anton.durcak22@gmail.com>
2022-05-17 20:05:22 +0200

    RMI product server done without observer

--- RentalSystem/src/server/networking/RMIServerProduct.java
+++ RentalSystem/src/server/networking/RMIServerProduct.java
@@ -1,5 +1,8 @@
 package server.networking;
 
+import server.model.ModelProxy;
+import server.model.product.ManageProducts;
+import server.model.product.ManageProductsManager;
 import shared.networking.ServerProduct;
 import shared.objects.product.*;
 import shared.util.Utils;
@@ -10,33 +13,40 @@ import java.rmi.server.UnicastRemoteObject;
 import java.util.ArrayList;
 
 public class RMIServerProduct implements ServerProduct {
+	private ManageProducts productsManager;
 
-	public RMIServerProduct() throws RemoteException {
-		UnicastRemoteObject.exportObject(this, Utils.SERVER_PORT);
+	public RMIServerProduct(ModelProxy modelProxy) {
+		this.productsManager = modelProxy.getManageProducts();
+
+		try {
+			UnicastRemoteObject.exportObject(this, Utils.SERVER_PORT);
+		} catch (RemoteException e) {
+			e.printStackTrace();
+		}
 	}
 
 	@Override
 	public void add(double price, Color color, EquipmentType equipmentType, Size size) {
-
+		productsManager.add(price, color, equipmentType, size);
 	}
 
 	@Override
 	public void remove(int index) {
-
+		productsManager.remove(index);
 	}
 
 	@Override
 	public Product getProduct(int index) {
-		return null;
+		return productsManager.getProduct(index);
 	}
 
 	@Override
 	public ProductList getAllProducts() {
-		return null;
+		return productsManager.getAllProducts();
 	}
 
 	@Override
 	public void changeProduct(int index, double newPrice, Color newColor, Size newSize) {
-
+		productsManager.changeProduct(index, newPrice, newColor, newSize);
 	}
 }
