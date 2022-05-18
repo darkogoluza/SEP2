35154e8016c047156fbd7e1695f4ba468d93de9c
xoxkoo <anton.durcak22@gmail.com>
2022-05-18 16:45:50 +0200

    user server model done

--- RentalSystem/src/server/model/ModelProxy.java
+++ RentalSystem/src/server/model/ModelProxy.java
@@ -1,13 +1,13 @@
 package server.model;
 
 import server.model.basket.ManageBasket;
-import server.model.customer.ManageCustomer;
 import server.model.product.ManageProducts;
 import server.model.reservation.ManageReservations;
+import server.model.user.ManageUser;
 
 public interface ModelProxy {
     ManageProducts getManageProducts();
     ManageReservations getManageReservations();
     ManageBasket getManageBasket();
-    ManageCustomer getManageCustomer();
+    ManageUser getManageUser();
 }
