b59b7c38b26c290c1d939505104309b500428b84
Viktor <viktor.vegh7@gmail.com>
2022-05-18 04:23:55 +0100

    updated the SingleOrderView.fxml

---
+++ RentalSystem/src/client/model/ManageBasketModel.java
@@ -0,0 +1,48 @@
+package client.model;
+
+import shared.objects.product.Product;
+
+import java.util.Map;
+
+public class ManageBasketModel
+{
+  void add(Product product)
+  {
+
+  }
+
+  Product remove(Product product)
+  {
+    return null;
+  }
+
+  void clear()
+  {
+
+  }
+
+  String getTotalPrice()
+  {
+    return null;
+  }
+
+  int size()
+  {
+    return 0;
+  }
+
+  Map<Product, Integer> getAllProductsByQuantity()
+  {
+    return null;
+  }
+
+  void order()
+  {
+
+  }
+
+  boolean isEmpty()
+  {
+    return false;
+  }
+}
---
+++ RentalSystem/src/client/model/ManageProductsModel.java
@@ -0,0 +1,42 @@
+package client.model;
+
+import shared.objects.product.*;
+import shared.util.PropertyChangeSubject;
+
+import java.beans.PropertyChangeListener;
+
+public class ManageProductsModel
+{
+  void add(double price, Color color, EquipmentType equipmentType, Size size)
+  {
+
+  }
+
+  void remove(String id)
+  {
+
+  }
+
+  Product getProduct(int index)
+  {
+    return null;
+  }
+
+  ProductList getAllProducts()
+  {
+    return null;
+  }
+
+  void changeProduct(int index, double newPrice, Color newColor, Size newSize)
+  {
+
+  }
+
+  void showAllProducts()
+  {
+
+  }
+
+}
+
+
